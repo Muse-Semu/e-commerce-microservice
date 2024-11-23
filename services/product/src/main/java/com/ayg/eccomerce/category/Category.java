30a84ea5e2f24714f0cccc3455d88d532e88ddcb
Muse Semu <muses@act.com.et>
2024-11-23 05:20:59 +0300

    add  category and product entity

--- services/product/src/main/java/com/ayg/eccomerce/category/Category.java
+++ services/product/src/main/java/com/ayg/eccomerce/category/Category.java
@@ -1,18 +1,18 @@
-package com.ayg.eccomerce.product;
+package com.ayg.eccomerce.category;
 
-import com.ayg.eccomerce.category.Category;
+import com.ayg.eccomerce.product.Product;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.math.BigDecimal;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,16 +20,13 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @Entity
-public class Product {
+public class Category {
 
     @Id
     @GeneratedValue
     private Integer id;
     private String name;
     private String description;
-    private double availableQuantity;
-    private BigDecimal price;
-    @ManyToOne
-    @JoinColumn(name = "category_id")
-    private Category category;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<Product> products;
 }
