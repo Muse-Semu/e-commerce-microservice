fafc20c9d1593dea4807bca2be11374286eb6fa8
Muse Semu <muses@act.com.et>
2024-11-28 06:33:57 +0300

    Notification mvc created

---
+++ services/notification/src/main/java/com/ayg/ecommerce/notification/Notification.java
@@ -0,0 +1,25 @@
+package com.ayg.ecommerce.notification;
+
+import com.ayg.ecommerce.kafka.order.OrderConfirmation;
+import com.ayg.ecommerce.kafka.payment.PaymentConfirmation;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Document
+public class Notification {
+
+    @Id
+    private String id;
+    private NotificationType type;
+    private LocalDateTime notificationDate;
+    private OrderConfirmation orderConfirmation;
+    private PaymentConfirmation paymentConfirmation;
+}
---
+++ services/notification/src/main/java/com/ayg/ecommerce/notification/NotificationType.java
@@ -0,0 +1,6 @@
+package com.ayg.ecommerce.notification;
+
+public enum NotificationType {
+    ORDER_CONFIRMATION,
+    PAYMENT_CONFIRMATION
+}
