d204291e85ee1d96eb2595738a6e10b2d1ebe82c
Muse Semu <muses@act.com.et>
2024-11-28 05:06:28 +0300

    NotificationProducer and notification request record created for payment service at notification sub module

---
+++ services/payment/src/main/java/com/ayg/ecommerce/notification/PaymentNotificationRequest.java
@@ -0,0 +1,16 @@
+package com.ayg.ecommerce.notification;
+
+
+import com.ayg.ecommerce.payment.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstname,
+        String customerLastname,
+        String customerEmail
+) {
+}
